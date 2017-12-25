9456fcfe5140b63e98824392d51d192ce1dba033
Georgios <georg.savvidis@yahoo.de>
2017-12-25 10:13:40 +0100

    modified Startscript so now hooks work again

--- BabyPi_start.sh
+++ BabyPi_start.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-mkdir -p /run/shm/rec/archive
+#rm -r /home/pi/picam/rec
+#rm -r /home/pi/picam/hooks
+#rm -r /home/pi/picam/state
+
+mkdir -p /run/shm/rec
 mkdir -p /run/shm/hooks
 mkdir -p /run/shm/state
+mkdir -p /home/pi/picam/archive
 
-ln -sfn /run/shm/rec/arvhive /home/pi/picam/archive
+ln -sfn home/pi/picam/archive /run/shm/rec/archive
+ln -sfn /run/shm/rec /home/pi/picam/rec
+ln -sfn /run/shm/hooks /home/pi/picam/hooks
+ln -sfn /run/shm/state /home/pi/picam/state
 
 cd /home/pi/node-rtsp-rtmp-server/
-./start_server.sh > /home/pi/rtmp-rtsp.log &
-sleep 40
-/home/pi/picam/picam --alsadev hw:1,0 --rtspout -w 800 -h 480 -v 500000 -f 20 > /home/pi/picam.log &
+./start_server.sh & # > /home/pi/rtmp-rtsp.log &
+sleep 60
+cd ..
+cd picam/
+./picam --alsadev hw:1,0 --rtspout  & # > /home/pi/picam.log &
 
